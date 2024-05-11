c85ce9e45014e4c682bc87f42d0de273561bc77e
coderprovider <white.pury1114@gmail.com>
2024-05-11 13:41:51 +0300

    feat: serve frontend app from go

--- main/server.go
+++ main/server.go
@@ -6,20 +6,12 @@ import (
 	"net/http"
 )
 
-func homePageHandler(w http.ResponseWriter, r *http.Request) {
-	_, err := fmt.Fprintf(w, "hello world")
-	checkError(err)
-}
-
-func checkError(err error) {
-	if err != nil {
-		log.Panic(err)
-	}
-}
-
 func main() {
-	http.HandleFunc("/", homePageHandler)
+	// Serve static files from the frontend/dist directory.
+	fs := http.FileServer(http.Dir("./frontend/dist"))
+	http.Handle("/", fs)
 
+	// Start the server.
 	fmt.Println("Server listening on port 3000")
 	log.Panic(
 		http.ListenAndServe(":3000", nil),
