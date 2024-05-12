cb411e50f9a173d2b7b2fd98567266466751cf8c
coderprovider <white.pury1114@gmail.com>
2024-05-12 11:08:26 +0300

    feat: add axios post button

--- frontend/src/components/TheWelcome.vue
+++ frontend/src/components/TheWelcome.vue
@@ -2,7 +2,21 @@
   <div class="welcome">
     This is a Vue frontend.
   </div>
+  <button @click="hello">
+    Say hello
+  </button>
 </template>
+
+<script setup lang="ts">
+import axios from 'axios';
+
+function hello() {
+  axios.post("http://localhost:3000/api/hello", "Hello from Vue")
+  .catch((error) => {
+    window.alert(`The API returned an error: ${error}`);
+  })
+}
+</script>
 
 <style scoped>
 .welcome {
