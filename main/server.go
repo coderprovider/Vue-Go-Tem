2be08eea4090b3df242fb11850f973bc5ca2c7f3
coderprovider <white.pury1114@gmail.com>
2024-05-16 12:07:11 +0300

    feat: add button handler to server

--- main/server.go
+++ main/server.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+func buttonHandler(w http.ResponseWriter, r *http.Request) {
+	var decoded string
+
+	// TODO: remove json decoder
+	err := json.NewDecoder(r.Body).Decode(&decoded)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Printf("Got the following message: %s\n", decoded)
+}
+
 func main() {
+	http.HandleFunc("/api/hello", buttonHandler)
+
 	// Serve static files from the frontend/dist directory.
 	fs := http.FileServer(http.Dir("./frontend/dist"))
 	http.Handle("/", fs)
